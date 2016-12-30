7fbc87df04838b3bbff0b39a8468d893b0ce5eb5
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2016-12-30 18:50:47 +0000

    better? colour mapping

--- hive.py
+++ hive.py
@@ -2,23 +2,34 @@ import unirest
 
 
 def rgb(minimum, maximum, value):
-    minimum, maximum = float(minimum), float(maximum)
-    ratio = 2 * (value-minimum) / (maximum - minimum)
-    b = int(max(0, 255*(1 - ratio)))
-    r = int(max(0, 255*(ratio - 1)))
-    g = 255 - b - r
+    mid_val = (maximum - minimum) / 2
+
+    if value > maximum:
+        value = maximum
+    if value < minimum:
+        value = minimum
+
+    if value > mid_val:
+        r = 255
+        g = 255 * ((maximum - value) / (maximum - mid_val))
+        b = 0
+    else:
+        g = 255
+        r = 255 * ((value - minimum) / (mid_val - minimum))
+        b = 0
+
     return r, g, b
 
 
 def get_hive_status(username, password):
-    login_response=unirest.post("https://api.hivehome.com/v5/login",
-                 headers={"Content-Type": "application/x-www-form-urlencoded" },
-                 params={"username": username, "password": password})
+    login_response = unirest.post("https://api.hivehome.com/v5/login",
+                                  headers={"Content-Type": "application/x-www-form-urlencoded"},
+                                  params={"username": username, "password": password})
 
     cookie = login_response.headers['Set-Cookie']
 
-    status_response=unirest.get("https://api.hivehome.com/v5/users/" + username + "/widgets/climate",
-                                headers={"Cookie": cookie})
+    status_response = unirest.get("https://api.hivehome.com/v5/users/" + username + "/widgets/climate",
+                                  headers={"Cookie": cookie})
 
     return status_response.body['active'], \
            status_response.body['currentTemperature'], \
